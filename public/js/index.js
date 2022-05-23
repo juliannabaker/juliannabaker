77cd31a9012f0b91a9502080e3138a8d9795c116
juliannabaker <jlb2886@g.rit.edu>
2022-05-23 13:16:34 -0600

    Adding Ready for Launch project and small updates to text

--- public/js/index.js
+++ public/js/index.js
@@ -4,13 +4,14 @@ $(function () {
 })
 
 const title = document.getElementById('job-title')
-const titles = ['Product Designer', 'Developer', 'UX Engineer', 'UX/UI Designer']
+const titles = ['Software Engineer', 'UX Engineer', 'UX/UI Designer', 'UX Researcher', 'Mobile Developer']
 let count = 0
 
 setInterval(() => {
   console.log('yo dawg')
+  console.log('you found me dawg')
   if (count === titles.length) count = 0
 
-  title.innerText = titles[count]
+  title.innerText = `${titles[count]}\n`
   count++
 }, 2000)
